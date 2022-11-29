4ee35a840d5347df3f6b3de3b8f9705c6be04de1
Micael Oliveira <micael.oliveira@anu.edu.au>
2022-11-29 14:01:04 +1100

    deseas: use new topography class.

--- src/deseas.f90
+++ src/deseas.f90
@@ -9,19 +9,17 @@ program deseas
   use iso_fortran_env
   use netcdf
   use utils
+  use topography
   use M_CLI2
   implicit none
 
   integer(int32) :: i, j, counter, its, its1, its2, sea_num, iblock, jblock, counter2
   integer(int32) :: im, ip, jm, jp, land
-  integer(int32) :: nxt, nyt                ! Size of model T grid
 
-  integer(int32) :: ncid_out, depth_id_out  ! NetCDF ids
-  integer(int32) :: ncid_topo, depth_id     ! NetCDF ids
-  integer(int32) :: dids_topo_out(2)        ! NetCDF ids
-  integer(int32) :: dids_topo(2)            ! NetCDF ids
+  type(topography_t) :: topog
 
-  real(real32), allocatable   :: depth(:,:)
+  integer(int32) :: ncid, sea_id, dids(2)  ! NetCDF ids
+
   integer(int16), allocatable :: sea(:,:)
   character(len=:), allocatable :: file_in, file_out
 
@@ -45,26 +43,19 @@ program deseas
   call check_file_exist(file_in)
 
   ! Get info on the grid from input
-  call handle_error(nf90_open(trim(file_in), nf90_nowrite, ncid_topo))
-  call handle_error(nf90_inq_dimid(ncid_topo, 'nx', dids_topo(1)))
-  call handle_error(nf90_inq_dimid(ncid_topo, 'ny', dids_topo(2)))
-  call handle_error(nf90_inquire_dimension(ncid_topo, dids_topo(1), len=nxt))
-  call handle_error(nf90_inquire_dimension(ncid_topo, dids_topo(2), len=nyt))
+  topog = topography_t(file_in)
 
-  call handle_error(nf90_inq_varid(ncid_topo, 'depth', depth_id))
-
-  allocate(depth(nxt,nyt),sea(nxt, nyt))
-  call handle_error(nf90_get_var(ncid_topo,depth_id, depth))
-  call handle_error(nf90_close(ncid_topo))
+  allocate(sea(topog%nxt, topog%nyt))
 
   ! Do
-  land = nxt + nyt + 1
+  write(*,*) "Removing seas"
+  land = topog%nxt + topog%nyt + 1
   sea = land
-  do j = 1, nyt
-    do i = 1, nxt
-      if (depth(i, j) > 0.0) sea(i, j) = i + j
+  do j = 1, topog%nyt
+    do i = 1, topog%nxt
+      if (topog%depth(i, j) > 0.0) sea(i, j) = i + j
     end do
-    if (all(depth(:, j) > 0.0)) sea(:, j) = 0  ! Southern Ocean all water across
+    if (all(topog%depth(:, j) > 0.0)) sea(:, j) = 0  ! Southern Ocean all water across
   end do
 
   do its = 1, 150   ! Only need high number after massive editing session with fjords. Normally 10 or so sweeps works.
@@ -72,18 +63,18 @@ program deseas
     sea_num = 1
 
     ! Get number of seas
-    do j = 2, nyt - 1
+    do j = 2, topog%nyt - 1
       i = 1
       jm = j - 1
       jp = j + 1
       if (sea(i, j) < land  .and. sea(i, j) > 0) then
         if (sea(i, j) >= sea_num) then
           sea(i, j) = sea_num
-          sea_num = max(min(sea_num+1, sea(nxt, j), sea(i, j-1), sea(i+1, j), sea(i, j+1)), sea_num)
+          sea_num = max(min(sea_num+1, sea(topog%nxt, j), sea(i, j-1), sea(i+1, j), sea(i, j+1)), sea_num)
         end if
       end if
 
-      do i = 2, nxt - 1
+      do i = 2, topog%nxt - 1
         im = i - 1
         ip = i + 1
         if (sea(i, j) < land  .and. sea(i, j) > 0) then
@@ -94,7 +85,7 @@ program deseas
         end if
       end do
 
-      i = nxt
+      i = topog%nxt
       if (sea(i, j) < land  .and. sea(i, j) > 0) then
         if (sea(i, j) >= sea_num) then
           sea(i, j) = sea_num
@@ -103,8 +94,8 @@ program deseas
       end if
     end do
 
-    j = nyt
-    do i = 2, nxt - 1
+    j = topog%nyt
+    do i = 2, topog%nxt - 1
       if (sea(i, j) < land  .and. sea(i, j) > 0) then
         if (sea(i,j) >= sea_num) then
           sea(i,j) = sea_num
@@ -116,17 +107,17 @@ program deseas
     ! Diffuse lowest values surrounding a point.
 
     ! Forward sweep.
-    do j = 2, nyt
+    do j = 2, topog%nyt
       jm = j - 1
-      jp = min(j+1, nyt)
+      jp = min(j+1, topog%nyt)
       i = 1
-      im = nxt
+      im = topog%nxt
       ip = 2
       if (sea(i, j) < land .and. sea(i, j) > 0) then
         sea(i,j) = min(sea(im, j), sea(ip, j), sea(i, jm), sea(i, jp))
         counter = counter + 1
       end if
-      do i = 2, nxt - 1
+      do i = 2, topog%nxt - 1
         im = i - 1
         ip = i + 1
         if (sea(i, j) < land .and. sea(i, j) > 0) then
@@ -144,7 +135,7 @@ program deseas
           counter = counter + 1
         end if
       end do
-      i = nxt
+      i = topog%nxt
       im = 1
       ip = i - 1
       if (sea(i, j) < land .and. sea(i, j) > 0) then
@@ -154,17 +145,17 @@ program deseas
     end do
 
     ! Backward sweep
-    do j = nyt, 2, -1
+    do j = topog%nyt, 2, -1
       jm = j - 1
-      jp = min(j+1, nyt)
+      jp = min(j+1, topog%nyt)
       i = 1
-      im = nxt
+      im = topog%nxt
       ip = 2
       if (sea(i, j) < land .and. sea(i, j) > 0) then
         sea(i,j) = min(sea(im, j), sea(ip, j), sea(i, jm), sea(i, jp))
         counter = counter + 1
       end if
-      do i = nxt - 1, 2, -1
+      do i = topog%nxt - 1, 2, -1
         im = i - 1
         ip = i + 1
         if (sea(i, j) < land .and. sea(i, j) > 0) then
@@ -182,7 +173,7 @@ program deseas
           counter = counter + 1
         end if
       end do
-      i = nxt
+      i = topog%nxt
       im = 1
       ip = i - 1
       if (sea(i, j) < land .and. sea(i, j) > 0) then
@@ -196,37 +187,27 @@ program deseas
     if (counter == 0 .or. sea_num == 1) exit
   end do
 
+  write(*,*) "Done"
+
   ! Write out new topography
-  do j = 1, nyt
-    do i = 1, nxt
-      if (sea(i, j) > 0) depth(i, j) = MISSING_VALUE
+  do j = 1, topog%nyt
+    do i = 1, topog%nxt
+      if (sea(i, j) > 0) then
+        topog%depth(i, j) = MISSING_VALUE
+        topog%frac(i, j) = MISSING_VALUE
+      end if
     end do
   end do
+  topog%lakes_removed = "yes"
+  call topog%write(file_out)
 
-
-  write(*,*) 'Wring'
-  call handle_error(nf90_create(trim(file_out), ior(nf90_netcdf4, nf90_clobber), ncid_out))
-  call handle_error(nf90_def_dim(ncid_out, 'nx', nxt, dids_topo_out(1)))
-  call handle_error(nf90_def_dim(ncid_out, 'ny', nyt,dids_topo_out(2)))
-  call handle_error(nf90_def_var(ncid_out, 'depth', nf90_float, dids_topo_out, depth_id_out, chunksizes=[nxt/10, nyt/10], &
+  call handle_error(nf90_create(trim('sea_num.nc'), ior(nf90_netcdf4, nf90_clobber), ncid))
+  call handle_error(nf90_def_dim(ncid, 'nx', topog%nxt, dids(1)))
+  call handle_error(nf90_def_dim(ncid, 'ny', topog%nyt, dids(2)))
+  call handle_error(nf90_def_var(ncid, 'sea_num', nf90_short, dids, sea_id, chunksizes=[topog%nxt/10, topog%nyt/10], &
     deflate_level=1, shuffle=.true.))
-  call handle_error(nf90_put_att(ncid_out, depth_id_out, 'missing_value', MISSING_VALUE))
-  call handle_error(nf90_put_att(ncid_out, depth_id_out, 'long_name', 'depth'))
-  call handle_error(nf90_put_att(ncid_out, depth_id_out, 'units', 'm'))
-  call handle_error(nf90_put_att(ncid_out, nf90_global, 'original_file', trim(file_in)))
-  call handle_error(nf90_put_att(ncid_out, depth_id_out, 'lakes_removed', 'yes'))
-  call handle_error(nf90_enddef(ncid_out))
-  write(*,*) 'putting'
-  call handle_error(nf90_put_var(ncid_out, depth_id_out, depth))
-  call handle_error(nf90_close(ncid_out))
-
-  call handle_error(nf90_create(trim('sea_num.nc'), ior(nf90_netcdf4, nf90_clobber), ncid_out))
-  call handle_error(nf90_def_dim(ncid_out, 'nx', nxt, dids_topo_out(1)))
-  call handle_error(nf90_def_dim(ncid_out, 'ny', nyt, dids_topo_out(2)))
-  call handle_error(nf90_def_var(ncid_out, 'sea_num', nf90_short, dids_topo_out, depth_id_out, chunksizes=[nxt/10, nyt/10], &
-    deflate_level=1, shuffle=.true.))
-  call handle_error(nf90_enddef(ncid_out))
-  call handle_error(nf90_put_var(ncid_out, depth_id_out, sea))
-  call handle_error(nf90_close(ncid_out))
+  call handle_error(nf90_enddef(ncid))
+  call handle_error(nf90_put_var(ncid, sea_id, sea))
+  call handle_error(nf90_close(ncid))
 
 end program deseas
